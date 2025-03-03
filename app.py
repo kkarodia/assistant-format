82e9ef1267472c9d02d46b82e78812949405512b
kkarodia <103606633+kkarodia@users.noreply.github.com>
2025-03-03 23:21:52 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import os
+import ast
+from apiflask import APIFlask
+from flask_httpauth import HTTPTokenAuth
+from dotenv import load_dotenv
+
+# Set how this API should be titled and the current version
+API_TITLE='Events API for Watson Assistant'
+API_VERSION='1.0.1'
+
+# create the app
+app = APIFlask(__name__, title=API_TITLE, version=API_VERSION)
+
+# load .env if present
+load_dotenv()
+
+# the secret API key, plus we need a username in that record
+API_TOKEN="{{'{0}':'appuser'}}".format(os.getenv('API_TOKEN'))
+#convert to dict:
+tokens=ast.literal_eval(API_TOKEN)
+
+# specify a generic SERVERS scheme for OpenAPI to allow both local testing
+# and deployment on Code Engine with configuration within Watson Assistant
+app.config['SERVERS'] = [
+    {
+        'description': 'Code Engine deployment',
+        'url': 'https://{appname}.{projectid}.{region}.codeengine.appdomain.cloud',
+        'variables':
+        {
+            "appname":
+            {
+                "default": "myapp",
+                "description": "application name"
+            },
+            "projectid":
+            {
+                "default": "projectid",
+                "description": "the Code Engine project ID"
+            },
+            "region":
+            {
+                "default": "us-south",
+                "description": "the deployment region, e.g., us-south"
+            }
+        }
+    },
+    {
+        'description': 'local test',
+        'url': 'http://127.0.0.1:{port}',
+        'variables':
+        {
+            'port':
+            {
+                'default': "5000",
+                'description': 'local port to use'
+            }
+        }
+    }
+]
+
+
+# set how we want the authentication API key to be passed
+auth=HTTPTokenAuth(scheme='ApiKey', header='API_TOKEN')
+
+# default "homepage", also needed for health check by Code Engine
+@app.get('/')
+def print_default():
+    """ Greeting
+    health check
+    """
+    # returning a dict equals to use jsonify()
+    return {'message': 'This is the certifications API server'}
+
+
+# Start the actual app
+# Get the PORT from environment or use the default
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=int(port))
